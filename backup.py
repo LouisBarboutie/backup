ed7aa08743006ea2570ab885d19c3ae48bb491ab
LouisB <louis.barboutie@gmail.com>
2025-10-30 14:00:20 +0100

    Improve error handling + Add check for available memory + Add early return if nothing needs to be backed up + Add generator for getting the paths of the files to back up

--- backup.py
+++ backup.py
@@ -3,6 +3,7 @@ import logging
 import sys
 from pathlib import Path
 from datetime import datetime, timedelta
+from typing import Iterator, Tuple
 
 SRC_PATHS = [
     Path("C:/Users/louis/Documents/"),
@@ -13,8 +14,6 @@ SRC_PATHS = [
 DST_PATH = Path("D:/backup/").expanduser().resolve()
 INTERVAL = timedelta(hours=6)
 
-available_space = shutil.disk_usage(DST_PATH).free
-print(shutil.disk_usage(SRC_PATHS[0]))
 
 logging.basicConfig(
     format="[{asctime}] {levelname:<8} - {message}",
@@ -27,24 +26,14 @@ logging.basicConfig(
     ],
 )
 
-size = 0
-for SRC_PATH in SRC_PATHS:
-    for src_path in SRC_PATH.rglob("*"):
-        if src_path.is_file():
-            size += src_path.stat().st_size
-    print(f"{size=}")
 
-logging.info("Starting backup...")
-
-for SRC_PATH in SRC_PATHS:
-    for src_path in SRC_PATH.rglob("*"):
-
+def files_to_backup_from(src_root: Path, dst_root: Path) -> Iterator[Tuple[Path, Path]]:
+    for src_path in src_root.rglob("*"):
         if not src_path.is_file():
             continue
 
-        rel_path = src_path.relative_to(SRC_PATH)
-        dst_path = DST_PATH / SRC_PATH.name / rel_path
-        dst_path.parent.mkdir(parents=True, exist_ok=True)
+        rel_path = src_path.relative_to(src_root)
+        dst_path = dst_root / src_root.name / rel_path
 
         if dst_path.exists():
             src_mtime = datetime.fromtimestamp(src_path.stat().st_mtime)
@@ -52,5 +41,42 @@ for SRC_PATH in SRC_PATHS:
             if src_mtime - dst_mtime < INTERVAL:
                 continue
 
-        logging.info(f"Backing up: {src_path}")
-        shutil.copy2(src_path, dst_path)
+        yield src_path, dst_path
+
+
+logging.info("Checking available space and backup size...")
+
+backup_size = 0
+file_count = 0
+for root in SRC_PATHS:
+    size = 0
+    for src_path, _ in files_to_backup_from(root, DST_PATH):
+        size += src_path.stat().st_size
+        file_count += 1
+    backup_size += size
+
+available_space = shutil.disk_usage(DST_PATH).free
+
+if file_count == 0:
+    logging.info("No files found to backup, exiting...")
+    sys.exit()
+
+logging.info(f"Found {file_count} file(s) to back up")
+logging.info(f"Size of back up: {backup_size} Bytes")
+logging.info(f"Available size: {available_space} Bytes")
+
+if backup_size >= available_space:
+    message = f"Not enough disk space available for the backup. Required {backup_size}, available {available_space}"
+    logging.error(message)
+    raise MemoryError(message)
+
+logging.info("Starting backup...")
+
+for SRC_PATH in SRC_PATHS:
+    for src_path, dst_path in files_to_backup_from(SRC_PATH, DST_PATH):
+        try:
+            dst_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy2(src_path, dst_path)
+        except Exception as e:
+            logging.error(f"Failed to back up {src_path}: {e}")
+        logging.info(f"Backed up: {src_path}")
